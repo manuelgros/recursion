76a8d38834a31c858eb89df60c4e253a3d66d4d1
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-07-31 12:01:21 -0700

    Formatting

--- fibonacci.rb
+++ fibonacci.rb
@@ -3,10 +3,10 @@
 def fibs n
   arr = [0, 1]
   if n <= 2
-    puts arr[..n-1]
+    puts arr[..n -1]
   else
     for i in 2..n-1
-      arr << arr[i-1] + arr[i-2]
+      arr << (arr[i-1] + arr[i-2])
     end
     arr
   end
@@ -15,9 +15,9 @@ end
 def fibs_rec n
   return [0] if n == 1
   return [0, 1] if n == 2
-  
-  array = fibs_rec(n-1)
-  array << array[-2] + array[-1]
+
+  array = fibs_rec(n - 1)
+  array << (array[-2] + array[-1])
 end
 
 p fibs 15
