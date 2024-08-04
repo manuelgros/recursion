ae9ac9f808e5c1e2aeba444bbd6260768c04b231
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-08-04 14:47:36 -0700

    Re-factored and cleaned up code

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,22 +1,22 @@
 # Iteratively and Recursive solution for the fibonacci sequence
 
-def fibs n
+def fibs(num)
   arr = [0, 1]
-  if n <= 2
-    puts arr[..n -1]
+  if num <= 2
+    puts arr[..num - 1]
   else
-    for i in 2..n-1
-      arr << (arr[i-1] + arr[i-2])
+    (2..num - 1).each do |idx|
+      arr << (arr[idx - 1] + arr[idx - 2])
     end
     arr
   end
 end
 
-def fibs_rec n
-  return [0] if n == 1
-  return [0, 1] if n == 2
+def fibs_rec(num)
+  return [0] if num == 1
+  return [0, 1] if num == 2
 
-  array = fibs_rec(n - 1)
+  array = fibs_rec(num - 1)
   array << (array[-2] + array[-1])
 end
 
